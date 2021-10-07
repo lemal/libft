2139b0009f1580622db09ad92e910ac98772f4c0
Tayna Pulaski <tapulask@ga-l5.msk.21-school.ru>
2021-10-07 17:33:35 +0300

    Working memset. Understood voids! Work with their addresses...

--- ft_memset.c
+++ ft_memset.c
@@ -13,25 +13,14 @@
 
 void	*ft_memset(void	*b, int	c, size_t	len)
 {
-	size_t			i;
-	unsigned char	temp_rev;
 	unsigned char	*temp;
 
-	i = 0;
 	temp = b;
-	while (i < len)
+	while (len > 0)
 	{
-		temp[i] = (c % 10) - '0';
-		c = c / 10;
-		i++;
+		*temp = (unsigned char)c;
+		temp++;
+		len--;
 	}
-	i = 0;
-	while (i < len)
-	{
-		temp_rev = temp[i];
-		temp[i] = temp[len - 1];
-		temp[len - 1] = temp_rev;
-		i++;
-	}
-	return (temp);
+	return (b);
 }
