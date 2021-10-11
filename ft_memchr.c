89e8dbb77208f3f09d9bf70abaecd49b55858cd7
Tayna Pulaski <tapulask@ga-l5.msk.21-school.ru>
2021-10-11 15:38:28 +0300

    ft_memchr ready

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,4 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memchr(const	void *s, int	c, size_t	n)
+{
+	size_t					i;
+	unsigned char			x;
+	const unsigned char		*str;
+
+	i = 0;
+	x = (unsigned char)c;
+	str = s;
+	while (i < n)
+	{
+		if (str[i] == x)
+			return ((void *)&str[i]);
+		i++;
+	}
+	return ((void *)0);
+}
