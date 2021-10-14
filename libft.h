19b12d6018b8ccc31317fc26428985da99a02704
lemal <tapulask@student.21-school.ru>
2021-10-14 10:05:05 +0300

    strlcpy and fixes

--- libft.h
+++ libft.h
@@ -34,7 +34,7 @@ int		ft_strncmp(const	char	*s1, const	char	*s2, size_t	n);
 void	*ft_memchr(const	void	*s, int	c, size_t	n);
 int		ft_memcmp(const	void *s1, const	void	*s2, size_t	n);
 char	*ft_strnstr(const	char	*haystack,
-			const	char	*needle, size_t	len);
+		const	char	*needle, size_t	len);
 int		ft_atoi(const	char	*str);
 char	*ft_itoa(int	n);
 #endif
