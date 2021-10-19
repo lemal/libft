61665f2089d1a20651a184e4b105efa00749f558
Tayna Pulaski <tapulask@ga-l5.msk.21-school.ru>
2021-10-19 15:06:18 +0300

    putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -10,7 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_putnbr_fd(int	n, int	fd)
 {
+	unsigned int	num;
 
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n *= -1;
+	}
+	num = (unsigned int)n;
+	if ((num / 10) != 0)
+		ft_putnbr_fd(num / 10, fd);
+	num = (num % 10) + '0';
+	write(fd, &num, 1);
 }
