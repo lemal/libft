12e8d5e833208a1fdd5ee2fc1c79f9b9fcbce219
Tayna Pulaski <tapulask@ga-l5.msk.21-school.ru>
2021-10-19 21:09:52 +0300

    memcmp to work on

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -19,6 +19,8 @@ int	ft_memcmp(const	void *s1, const	void	*s2, size_t	n)
 
 	temp_1 = s1;
 	temp_2 = s2;
+	if (!n)
+		return (0);
 	while (((*temp_1 != '\0') || (*temp_2 != '\0')) && (n > 0))
 	{
 		if (*temp_1 < *temp_2)
@@ -33,3 +35,23 @@ int	ft_memcmp(const	void *s1, const	void	*s2, size_t	n)
 		return ((int)(*temp_1 - *temp_2));
 	return (0);
 }
+/*
+size_t				i;
+	const unsigned char	*temp_1;
+	const unsigned char	*temp_2;
+	
+	i = 0;
+	temp_1 = s1;
+	temp_2 = s2;
+	if (!n)
+		return (0);
+	while (((temp_1[i] != '\0') && (temp_2[i] != '\0')) && (n > i))
+	{
+		if ((temp_1[i] < temp_2[i]) || (temp_1[i] > temp_2[i]))
+			return ((int)(temp_1[i] - temp_2[i]));
+		i++;
+	}
+	// if (*temp_1 == '\0' || *temp_2 == '\0')
+	// 	return ((int)(*temp_1 - *temp_2));
+	return (0);
+*/
