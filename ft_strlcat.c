27dca7d6674e21c69ab3b1579269d4f3e9991932
Tayna Pulaski <tapulask@ga-l3.msk.21-school.ru>
2021-10-21 14:42:17 +0300

    strlcat no comment

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -39,34 +39,3 @@ size_t	ft_strlcat(char	*dst, const	char	*src, size_t	dstsize)
 	dst[storage.i] = '\0';
 	return (storage.i - storage.j + storage.count_src);
 }
-/*
-	size_t	count_src;
-	size_t	count_dst;
-	size_t	i;
-	size_t	j;
-	size_t	dst_temp;
-
-	dst_temp = dstsize;
-	i = 0;
-	j = 0;
-	count_src = 0;
-	count_dst = 0;
-	while (src[count_src])
-		count_src++;
-	while (dst[count_dst] && dst_temp != 0)
-	{
-		count_dst++;
-		dst_temp--;
-	}
-	i += count_dst;
-	if (dst_temp == 0)
-		return (count_src + count_dst);
-	while (i < (dstsize - 1) && src[j])
-	{
-		dst[i] = src[j];
-		i++;
-		j++;
-	}
-	dst[i] = '\0';
-	return (i - j + count_src);
-*/
